8ea023d29fabf8a8127786dcdd8f6599826870d3
Dhanraj-bidchat <dhanraj.naik@bidchat.com>
2020-11-09 22:15:01 +0530

    -Added safearea to handle complex device shape. -Container is as good as View from RN -margins and padding are set using EdgeInsets

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,17 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Container(
+            color: Colors.amber[900],
+            child: Text('HELLO BOY'),
+            height: 200,
+            width: 200,
+            margin: EdgeInsets.symmetric(vertical: 15, horizontal: 10),
+            padding: EdgeInsets.all(10.0),
+            alignment: Alignment.center,
+          ),
+        ),
       ),
     );
   }
